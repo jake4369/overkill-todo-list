882ae27f74325ef7bd21b722e6ea49309018f806
Jake Samuels <samuels242@googlemail.com>
2023-11-05 11:19:17 +0000

    Fix bug

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -16,48 +16,75 @@ const storage = multer.diskStorage({
   },
 });
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
+  const mimetype = mimetypes.test(file.mimetype);
+
   if (extname && mimetype) {
-    return cb(null, true);
+    cb(null, true);
   } else {
-    cb("Images only!");
+    cb(new Error("Invalid file type"), false);
   }
 }
 
 const upload = multer({
   storage,
+  fileFilter,
+});
+
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    } else {
+      // Check if a file was uploaded
+      if (!req.file) {
+        return res.status(400).json({ message: "No image uploaded" });
+      }
+
+      // Use sharp to resize the image to 100x100 pixels
+      sharp(req.file.path)
+        .resize(100, 100)
+        .toFile(`uploads/resized-${req.file.filename}`, (err, info) => {
+          if (err) {
+            return res.status(500).json({ message: "Error resizing image" });
+          }
+
+          res.json({
+            message: "Image Uploaded and Resized",
+            image: `/${req.file.path}`,
+            resizedImage: `/uploads/resized-${req.file.filename}`,
+          });
+        });
+    }
+  });
 });
 
 // router.post("/", upload.single("image"), (req, res) => {
-//   res.send({
-//     message: "Image Uploaded",
-//     image: `/${req.file.path}`,
-//   });
+//   // Check if a file was uploaded
+//   if (!req.file) {
+//     return res.status(400).json({ message: "No image uploaded" });
+//   }
+
+//   // Use sharp to resize the image to 100x100 pixels
+//   sharp(req.file.path)
+//     .resize(100, 100)
+//     .toFile(`uploads/resized-${req.file.filename}`, (err, info) => {
+//       if (err) {
+//         return res.status(500).json({ message: "Error resizing image" });
+//       }
+
+//       res.json({
+//         message: "Image Uploaded and Resized",
+//         image: `/${req.file.path}`,
+//         resizedImage: `/uploads/resized-${req.file.filename}`,
+//       });
+//     });
 // });
-
-router.post("/", upload.single("image"), (req, res) => {
-  // Check if a file was uploaded
-  if (!req.file) {
-    return res.status(400).json({ message: "No image uploaded" });
-  }
-
-  // Use sharp to resize the image to 100x100 pixels
-  sharp(req.file.path)
-    .resize(100, 100)
-    .toFile(`uploads/resized-${req.file.filename}`, (err, info) => {
-      if (err) {
-        return res.status(500).json({ message: "Error resizing image" });
-      }
-
-      res.json({
-        message: "Image Uploaded and Resized",
-        image: `/${req.file.path}`,
-        resizedImage: `/uploads/resized-${req.file.filename}`,
-      });
-    });
-});
 
 export default router;
